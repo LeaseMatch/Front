ad698327e1d1b07e5f49bb019059e9fe0eead0b0
Elio Cortés <elio.cortes3000@gmail.com>
2024-11-27 12:53:22 -0500

    Updated url

--- src/components/PropertiesList.tsx
+++ src/components/PropertiesList.tsx
@@ -32,7 +32,7 @@ const PropertiesList: React.FC = () =>
         try
         {
             const response = await axios.get(
-                `https://mwo14j15q1.execute-api.us-east-1.amazonaws.com/prod/properties`,
+                `https://12mo5zkgvg.execute-api.us-west-2.amazonaws.com/prod/properties`,
                 {
                     params: {
                         city: cityQuery,
